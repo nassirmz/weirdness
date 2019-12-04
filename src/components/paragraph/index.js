c5eeeee30451fa8665d800253008513b77fb81c2
nassir <nassirmz@gmail.com>
2019-12-04 14:06:02 -0500

    Add paragraph component'

---
+++ src/components/paragraph/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Paragraph = () => {
+  return (
+    <Container>
+      <p>
+        Find out how weird you are by selecting the GIFs that make you laugh.
+        We'll show you the least weird ones to start, but you can move the slider to make them weirder.
+      </p>
+      <p>
+        When you find a GIF you like press the LIKE button. Once you like 5 GIFs,
+        we'll show you how weird you are.
+      </p>
+    </Container>
+  );
+};
+
+export default Paragraph;

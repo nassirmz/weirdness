c2fd8065b0efee91302d3a948d3665dd0cc50094
nassir <nassirmz@gmail.com>
2019-12-04 13:22:57 -0500

    Add header component

---
+++ src/components/header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand>
+        Weirdness Calculator
+      </Navbar.Brand>
+    </Navbar>
+  );
+};
+
+export default Header;
